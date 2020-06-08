c8cf386ee8ace049f530e091e120a59ddb8431ab
nbshinde <64580380+nbshinde@users.noreply.github.com>
2020-06-08 21:37:16 +0530

    Create tests.py

---
+++ Flask_Blog/tests.py
@@ -0,0 +1,25 @@
+import unittest
+from words import OxfordDictionaries
+
+class API_Tester(unittest.TestCase):
+    def setUp(self):
+        self.oxford = OxfordDictionaries(app_id, app_key)
+
+    def test_synonyms(self):
+        self.assertEqual(self.oxford.get_synonyms("play").status_code, 200)
+
+    def test_antonyms(self):
+        self.assertEqual(self.oxford.get_antonyms("play").status_code, 200)
+
+    def test_antonyms(self):
+        self.assertEqual(self.oxford.translate("play").status_code, 200)
+
+    def test_antonyms(self):
+        self.assertEqual(self.oxford.get_info_about_word("play").status_code, 200)
+
+    def test_antonyms(self):
+        self.assertEqual(self.oxford.use_in_sentence("play").status_code, 200)
+
+if __name__ == '__main__':
+    unittest.main()
+        
