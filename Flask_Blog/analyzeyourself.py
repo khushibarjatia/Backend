3aa62c3c29cee3c98b13cc4c4da0b00befe0345a
nbshinde <64580380+nbshinde@users.noreply.github.com>
2020-06-15 20:16:06 +0530

    Create analyzeyourself.py

---
+++ Flask_Blog/analyzeyourself.py
@@ -0,0 +1,543 @@
+q1 = """ Select the antonym for word light:
+   a.Heavy
+   b.hard
+   c.tendy
+   d.brighter"""
+q2 = """Rhyming word for try:
+   a.tree
+   b.grey
+   c.fry
+   d.whee"""
+q3 = """ Were there any beers in the refrigerator?
+   a.Yes,there will be a few.
+   b.Thereare a little.
+   c.There was not even one.
+   d.There have been some.
+   e.There were a little."""
+q4 = """Where are my car keys?
+      They are _ _ _ _your hands!
+   a.on
+   b.to
+   c.of
+   d.in"""
+q5 = """Whose keys are these?
+      _ _ _ _are mine.
+   a.They
+   b.These 
+   c.It
+   d.whose
+   e.keys"""
+q6 = """_ _ _ the war ,most Italians were farmers.
+   a.To
+   b.From
+   c.While
+   d.Before
+   e.When"""
+q7 ="""who is your mother's sister's daughter?
+      _ _ _ _ .
+   a.She is my wife.
+   b.She is my mother's nephew.
+   c.She  my cousin.
+   d.Is my mother's niece.
+   e.My mother's sister's daughter is my cousin."""
+q8 ="""Paul can't touch ceiling because he is too _ _ _.
+   a.shortening
+   b.shortness
+   c.shorten
+   d.shortly
+   e.short."""
+q9 =""" Is there a doctor in the house?
+    _ _ _ _.
+   a.Yes,is there.
+   b.No,isn't.
+   c.Yes,there is.
+   d.No,they isn't
+   e.No ,theirs not."""
+q10 =""" We must _ _ _ _ the train at the next stop.
+   a.get off
+   b.get over
+   c.get on
+   d.get down
+   e.get up"""
+q11 = """Put your arms _ _ _ _ me and kiss me you fool!
+   a.beyond
+   b.around
+   c.to
+   d.onto
+   e.on"""
+q12 = """English grammmer is the worst grammmer of  any language.
+       No,it isn't.German grammer _ _ _ .
+   a.worse is.
+   b.is worst.
+   c.is bladder.
+   d.is worse."""
+q13 = """ Is that boy Mary's son? _ _ _.
+   a.Yes,name is Robert.
+   b.Yes,he is.
+   c.No,he is Marys nephew.
+   d.Yes,he are."""
+q14 = """ Where are you going?
+   a.I am going to Johns house.
+   b.I going at work.
+   c.I are going the store.
+   d.I am going to my mother's house."""
+q15 = """ He was invited_ _ _ _ he did not come.
+   a.if
+   b.when
+   c.or
+   d.but
+   e.while."""
+q16 = """ They _ _ _ _ the pyramids when went to Egypt.
+   a.didn't see.
+   b.seed
+   c.did not saw
+   d.not see
+   e.not seen"""
+q17 = """ Have you ever _ _ _ in that restaurant?
+   a.eat
+   b.going
+   c.ate 
+   d.went
+   e.eaten."""
+q18 =""" I will speak _ _ _ Suzanne when I see her.
+   a. in
+   b.to
+   c.around
+   d.at
+   e.toward"""
+q19 = """ Edward has always _ _ _ things very quickly and efficiently.
+   a.does
+   b.done 
+   c..did
+   d.do"""
+q20 = """Neha been working there _ _ _  only eight months.
+   a.in
+   b.by
+   c.for
+   d.since
+   e.from."""
+q21 =""" The police went_ _ _ _ all of Karl's things, but they didn't find any guns.
+   a.under
+   b.past
+   c.through
+   d.in
+   e.across."""
+q22 = """These pants _ _ _ mine;that jackets is yours.
+   a.of
+   b.are wearing
+   c.are
+   d.is wearing
+   e.is."""
+q23 =""" Never look directly _ _ _ the sun.It is bad for your eyes
+   a.to
+   b.for
+   c.through
+   d.of
+   e.at"""
+q24 ="""You can use my car _ _ _  tomorrow.
+   a.yet
+   b.since
+   c.until
+   d.around
+   e.in"""
+q25 ="""She _ _ _ blue velvet to the party last night.
+   a.worn
+   b.war
+   c.weared
+   d.wear
+   e.wore"""
+q26 = """_ _  _he understand what you were talking about?
+   a.could
+   b.cans
+   c.can't
+   d.does"""
+q27 ="""My sister _ _ _play tennis now.
+   a.can to
+   b.can
+   c.will can"""
+q28 = """I _ _ _ walk when I was less than a year old.
+   a.can
+   b.could
+   c.have can"""
+q29 = """(polite)_ _ _ you tell me what time it is ,please?
+   a.could
+   b.can
+   c.will"""
+q30 ="""I _ _ _ _ _  play Basketball .Use appropriate conjunction for past tense.
+   a.was
+   b.used to 
+   c.were
+   d.am going to """
+q31 =""" Where _ _ _the Battle of the Bulge?
+   a.was
+   b.are
+   c.is
+   d.were
+   e.weren't"""
+q32 = """ They're tired _ _ _ they worked for ten hours.
+   a.because
+   b.however
+   c.or
+   d.when
+   e.except"""
+q33 ="""Excuse me,where is the post office?
+        I'm sorry ,I _ _ _ _English.
+   a.am not speaking 
+   b.speak not
+   c.no speak
+   d.don't speak"""
+q34 ="""I told _ _ _ _you didn't listen.
+   a.because
+   b.but
+   c.for
+   d.then
+   e.if"""
+q35 = """We studied the question _ _ _ _.
+   a.scientific
+   b.science
+   c.sciencing
+   d.scientifically."""
+q36 ="""Don't count your chickens _ _ _they're hatched.
+   a.then
+   b.before
+   c.and
+   d.but
+   e.or"""
+q37 ="""I'm sorry,you will just have to _ _ _.
+   a.wait
+   b.avoid
+   c.like
+   d.attempt
+   e.find."""
+q38 ="""The little dog _ _ _ my leg.
+   a.bited
+   b.bitted
+   c.bit
+   d.bitten
+   e.bite"""
+q39 ="""_ _ _ _ my wife sleeps, I watch TV late at night.
+   a.Though
+   b.During
+   c.From
+   d.While
+   e.By"""
+q40 ="""The milk is _ _ _ the refrigerator.
+   a.to
+   b.of
+   c.at
+   d.in
+   e.from"""
+q41 = """Thomas can't get out of bed because he _ _ _ _his leg.
+   a.breakable
+   b.break
+   c.breaks
+   d.broken
+   e.broke"""
+q42 ="""I have never _ _ _ such a boring book!
+   a.saw
+   b.read
+   c.readed
+   d.red
+   e.readen"""
+q43 ="""Choose synonym for Huge:
+   a.small
+   b.vast
+   c.little
+   d.tiny"""
+q44 ="""Choose antonym for dry:
+   a.cold
+   b.tough
+   c.hard
+   d.wet"""
+q45:"""Choose antonym for cheap:
+   a.dirty
+   b.inexpensive
+   c.costly
+   d.valuable"""
+q46:"""Choose antonym for dirty:
+   a.ugly
+   b.clean
+   c.cheap
+     """
+q47:"""I am American.I am from Boston._ _ _ hair is brown .
+   a.The 
+   b.My 
+   c.Mine
+   d.I
+   e.These"""
+q48:"""Had they _ _ _ arrived when you got there?
+   a.after
+   b.before
+   c.still
+   d.already
+   e.yet"""
+q49:"""She cannot talk _ _ _ _ walk at the same time.
+   a.and
+   b.because
+   c.if
+   d.except
+   e.while"""
+q50:"""I'm surprised because rain was not _ _ _ in the weather report.
+   a.prediction
+   b.predicted
+   c.predictable
+   d.predictably
+   e.predict"""
+
+
+q51:"""Some birds are sitting _ _ _the house.
+   a.onto
+   b.for
+   c.to
+   d.after
+   e.on the top of"""
+q52:"""After this rain ,tomorrow _ _ _ be sunny.
+   a.ought to
+   b.should to
+   c.can
+   d.is
+   e.would"""
+
+q53:"""Choose the correct response:
+      Can you run 26 miles?
+   a.No,I cannot ;it is far enough.
+   b.No ,I can't.
+   c.Yes I do a lot.
+   d.Yes,I can't.
+   e.No,I cannot .I am not so strong."""
+q54:"""Plural form of knife:
+   a.knifes
+   b.knif
+   c.knifen
+   d.knive
+   e.knives"""
+q55:"""Very good class,today _  _was late.
+   a.someone
+   b.everyone
+   c.anyone
+   d.no one
+   e.this one"""
+q56:"""Veronica _ _ _him very much .
+   a.spoke
+   b.looked
+   c.waited
+   d.hated
+   e.talked"""
+q57:"""I'm so sorry but Ms.Patel has already _ _ _ for the day.
+   a.went away
+   b.to depart
+   c.left
+   d.go
+   e.went"""
+q58:"""If it rains tomorrow,_ _ _ _ .
+   a.we would stay home
+   b.they will must bring umbrellas.
+   c.they will take car.
+   d.we won't go.
+   e.they would have come back."""
+
+q59:"""I told my teacher that I _ _ _ what she said.
+   a.did not believe
+   b.was not believing"""
+q60:"""I _ _ _the number 12 bus just down the road .
+   a.could see
+   b.was seeing"""
+q61:"""At a moment Maria _ _ _ her homework ,as se does every day.
+   a.does
+   b.is doing"""
+q62:""" The baby _ _ _ 21 inches.
+   a.measures
+   b.is measuring"""
+q63:"""Please be quite.I _ _ _  to radio.
+   a.listen
+   b.am listening
+   c.listened"""
+q64:"""Everything on the menu _ _ _delicious.
+   a.tastes
+   b.is tasting"""
+q65:""" Why_ _ _ so selfish about this?
+   a.are you
+   b.are you being
+   c.you being."""
+q66:"""Was he on time or was he _ _ _ ?
+   a.late
+   b.being late"""
+q67:""" He told the police he'd only had a small drink and was _ _ _not to drive too fast.
+   a.careful
+   b.being careful"""
+q68:""" Is she always _ _ _ with children and animals?
+   a.so patient
+   b.being so patient"""
+q69:"""Vriddhi said to Aryan that he is _ _ _  her.
+   a.smart than
+   b.smarter than """
+q70:"""Akshay told Prasad _  _ _ _about him.
+   a.to worry
+   b.to don't worry
+   c.don't worry
+   d.not to worry"""
+q71:"""_ _ _ Khushi _ _  Prachiti are working together.
+   a. when,and
+   b.both,and"""
+q72:"""Neha _ _ _ Sakshi are best friends from their childhood.
+   a.and
+   b.or"""
+q73:"""_ _ _milk you drink everyday?
+   a.how many
+   b.how much"""
+q74:"""She did it _ _ her own .
+   a.by
+   b.on"""
+q75:"""It is not _ _ _ mine.
+   a.same as
+   b.same like
+   c.the same as
+   d.the same like"""
+q76:"""I didn't see _ _ _ people there.
+   a.many
+   b.many of"""
+q77:"""What _ _ _you like for dinner tonight?
+   a.do
+   b.would
+   c.could"""
+q78:"""She did it _ _ _ Tuesday.
+   a.last
+   b.on last"""
+q79:"""Choose the similar word for the word: peace
+   a.calm
+   b.elysian"""
+q80:"""The radio was _ _ _ from my car last week.
+   a.robbed
+   b.stolen"""
+q81:"""The person _ _ _ interviewed me was very friendly.
+   a.who
+   b.which"""
+q82:"""_ _ _ not the way to do it.
+   a.that's 
+   b.there's
+   c.Either could be used here."""
+q83:""" _ _ _no way to do it.
+   a.That's
+   b.There's
+   c.Either could be used here."""
+q84:"""I was certain _ _ _she would be there.
+   a.of
+   b.that
+   c.whether"""
+q85:"""If I go ,I  _ _ speak to her.
+   a.will
+   b.would"""
+q86:"""If I went,I_ _ _ speak to her.
+   a.will
+   b.would"""
+q87:"""I'll be _ _ _.
+   a.their
+   b.there"""
+q88:"""I haven't seen her _ _ _ ages.
+   a.for
+   b.since"""
+q89:"""choose the correct spelling:
+   a.tommorrow
+   b.tomorrow
+   c.tommarow      """
+q90:"""That was rude-you _ _ _ have done it.
+   a.mustn't
+   b.shouldn't"""
+q91:"""She found it easy _ _ _the test,
+   a.finish
+   b.to finish
+   c.finishing"""
+q92:"""Choose the correct spelling:
+   a.neseccery
+   b.neccessary
+   c.neccesary
+   d.necessary"""
+q93:"""Choose the correct spelling:
+   a.succes
+   b.success
+   c.sucess"""
+q94:"""It looks _ _ _ _good,
+   a.pretty
+   b.prettily""" 
+q95:"""She's good _ _ _ maths.
+   a.in
+   b.at
+   c.on"""
+q96:"""Vegetables and fruits are good _ _ _ you.
+   a.to
+   b.by
+   c.for
+   d.in"""
+q97:"""She spoke _ _ _ him yesterday.
+   a.to
+   b.with
+   c.for"""
+q98:"""If I'd been there,I _ _ _that.
+   a.wouldn't do
+   b.wouldn't have done """
+q99:"""It was _ _ _ hot to drink.
+   a.so
+   b.such
+   c.too
+   d.very"""
+q100:"""She dropped her kids _ _ _ at school on her way to work.
+   a.of
+   b.off
+   c.up
+   d.to"""
+q101:"""It _ _  _nation of millions to hold us back.
+   a.makes
+   b.takes"""
+q102:"""There are only fifteen questions _ _ _ _ in this test.
+   a.go 
+   b.going
+   c.to go"""
+q103:"""I'll look it  _ _ _in the dictionary. 
+   a.in
+   b.on
+   c.at
+   d.to
+   e.up"""
+q104:"""It's _  _ _trouble.
+   a.too much
+   b.too many"""
+q105:"""Amartya climbed _ _ _ _ Everest.
+   a.Mount
+   b.the Mount
+   c.The Mount"""
+q106:"""The shop is in_ _ _ street.
+   a.Oxford
+   b.the Oxford"""
+q107:"""If I move to the country ,it wil mean _ _ _a long way every day.
+   a.travel
+   b.travelling
+   c.to travel"""
+q108:"""Has Neha _ _ _ to him about it?
+   a.said
+   b.spoken
+   c.told"""
+questions ={q1:"a",q2:"c",q3:"e",q4:"d",q5:"a",q6:"d",q7:"e",q8:"e",q9:"c",q10:"a",q11:"b",q12:"d",q13:"b",q14:"d",q15:"d",q16:"a",
+            q17:"e",q18:"b",q19:"b",q20:"c",q21:"c",q22:"c",q23:"e",q24:"c",q25:"e",q26:"a",q27:"b",q28:"b",q29:"c",q30:"b",q31:"a"
+            ,q32:"a",q33:"d",q34:"b",q35:"d",q36:"b",q37:"a",q38:"e",q39:"d",q40:"d",q41:"e",q42:"b",q43:"b",q44:"d",q45:"c",q46:"b",
+            q47:"b",q48:"d",q49:"a",q50:"b",q51:"e",q52:"e",q53:"b",q54:"e",q55:"d",q56:"d",q57:"c",q58:"d",q59:"a",q60:"a",q61:"b",
+            q62:"a",q63:"b",q64:"a",q65:"b",q66:"a",q67:"b",q68:"a",q69:"b",q70:"d",q71:"b",q72:"a",q73:"b",q74:"b",q75:"c",q76:"a",
+            q77:"b",q78:"a",q79:"b",q80:"b",q81:"a",q82:"a",q83:"b",q84:"b",q85:"a",q86:"b",q87:"b",q88:"a",q89:"b",q90:"b",q91:"b",q92:"d",q93:"b",
+            q94:"a",q95:"b",q96:"c",q97:"b",q98:"b",q99:"c",q100:"a",q101:"b",q102:"c",q103:"e",q104:"a",q105:"a",q106:"a",q107:"b",q108:"b"
+            }
+score = 0
+for i in questions:
+    print(i)
+    flag1 =input("Do you want to skip this question? Y/N")
+    if flag1=="Y":
+       continue
+    ans = input("Enter the answer:")
+    if ans==questions[i]:
+        print("correct answer,you got 1 point")
+        score = score+1
+    else:
+        print("wrong answer,you lost 1 point")
+        score =score-1
+    flag2 =input("Do you want to quit ? Y/N")
+    if flag2 =="y":
+       break
+print("FInal score is:" ,score)
