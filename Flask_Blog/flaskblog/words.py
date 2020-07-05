b56074ff8f43e33bcb2b005e28fcea5cecef318f
nbshinde <64580380+nbshinde@users.noreply.github.com>
2020-07-05 16:51:16 +0530

    Create words.py

---
+++ Flask_Blog/flaskblog/words.py
@@ -0,0 +1,182 @@
+words = [
+{
+    "word_id": "1",
+    "word": "NONPLUSSED:",
+    "meaning": "Filled with bewilderment"
+},
+{
+    "word_id": "2",
+    "word": "INCHOATE:",
+    "meaning": "Only partly in existence; imperfectly formed"
+},
+{
+    "word_id": "3",
+    "word": "MARTINET:",
+    "meaning": "Someone who demands complete obedience; a strict disciplinarian."
+},
+{
+    "word_id": "4",
+    "word": "HOI POLLOI:",
+    "meaning": "Common people"
+},
+{
+    "word_id": "5",
+    "word": "ABJECT:",
+    "meaning": "Completely without pride or dignity."
+},
+{
+    "word_id": "6",
+    "word": "ABERRATION:",
+    "meaning": "A state or condition markedly different from the norm."
+},
+{
+    "word_id": "7",
+    "word": "ABJURE:",
+    "meaning": "Formally reject a formerly held belief."
+},
+{
+    "word_id": "8",
+    "word": "ABROGATE:",
+    "meaning": "revoke formally"
+},
+{
+    "word_id": "9",
+    "word": "ABSCOND:",
+    "meaning": "Run away; often taking something or somebody along"
+},
+{
+    "word_id": "10",
+    "word": "ABSTRUSE:",
+    "meaning": "Difficult to understand"
+},
+{
+    "word_id": "11",
+    "word": "ALACRITY:",
+    "meaning": "Liveliness and eagerness"
+},
+{
+    "word_id": "12",
+    "word": "ALIAS:",
+    "meaning": "A name that has been assumed temporarily."
+},
+{
+    "word_id": "13",
+    "word": "ANACHRONISTIC:",
+    "meaning": "Chronologically misplaced"
+},
+{
+    "word_id": "14",
+    "word": "ANATHEMA:",
+    "meaning": "A formal eccelesiastical curse accompanied by excommunication."
+},
+{
+    "word_id": "15",
+    "word": "ANNEX:",
+    "meaning": "Attach to"
+},
+{
+    "word_id": "16",
+    "word": "ANTEDILUVIUM:",
+    "meaning": "Of or relating to the period before the biblical flood."
+},
+{
+    "word_id": "17",
+    "word": "APATHETIC:",
+    "meaning": "Showing little or no emotion"
+},
+{
+    "word_id": "18",
+    "word": "ANTITHESIS:",
+    "meaning": "Exact opposite"
+},
+{
+    "word_id": "19",
+    "word": "APOCRYPHAL:",
+    "meaning": "Of doubtful authenticity"
+},
+{
+    "word_id": "20",
+    "word": "CACHET AND PANACHE:",
+    "meaning": "An indication of approved or superior status; distinctive and stylish respectively, CACHET is more about prestige and PANACHE is more about style."
+},
+{
+    "word_id": "21",
+    "word": "INDEFATIGABLE:",
+    "meaning": "Showing sustained enthusiastic action with unflagging vitality."
+},
+{
+    "word_id": "22",
+    "word": "UNCANNY:",
+    "meaning": "Surpassing the ordinary or normal."
+},
+{
+    "word_id": "23",
+    "word": "UNABASHED:",
+    "meaning": "Not embarrased"
+},
+{
+    "word_id": "24",
+    "word": "DILATORY:",
+    "meaning": "Wasting time"
+},
+{
+    "word_id": "25",
+    "word": "ACCOST:",
+    "meaning": "Approach and speak to someone aggressively"
+},
+{
+    "word_id": "26",
+    "word": "ACCRETION:",
+    "meaning": "An increase by natural growth or addition"
+},
+{
+    "word_id": "27",
+    "word": "ACUMEN:",
+    "meaning": "Shrewdness shown by keen insight"
+},
+{
+    "word_id": "28",
+    "word": "ADAMANT:",
+    "meaning": "very sure; refusing to change one's mind"
+},
+{
+    "word_id": "29",
+    "word": "ADMONISH:",
+    "meaning": "Scold or reprimand; take to task"
+},
+{
+    "word_id": "30",
+    "word": "ADUMBRATE:",
+    "meaning": "Describe roughly or give the main points or summary"
+},
+{
+    "word_id": "31",
+    "word": "AFFLUENT:",
+    "meaning": "Having an abundant supply of money or possessions of value."
+},
+{
+    "word_id": "32",
+    "word": "AGGRANDIZE:",
+    "meaning": "Increase the scope, power or importance of"
+},
+{
+    "word_id": "33",
+    "word": "AMBIVALENT:",
+    "meaning": "Uncertain or unable to decide about what course to follow."
+},
+{
+    "word_id": "34",
+    "word": "AMENABLE:",
+    "meaning": "Disposed or willing to comply"
+},
+{
+    "word_id": "35",
+    "word": "ARROGATE:",
+    "meaning": "Seize and take control without authority."
+},
+{
+    "word_id": "36",
+    "word": "ASCETIC:",
+    "meaning": "Someone who practices self denial as a spiritual discipline."
+}
+]
